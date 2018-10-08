7e893c1192fed6508d4e1c7fa9de2948156e117f
gaosx <gaosx@cecurs.com>
2018-10-08 16:51:22 +0800

    add httputil

--- xiong/src/main/java/http/HttpUtil.java
+++ xiong/src/main/java/http/HttpUtil.java
@@ -1,17 +1,7 @@
 package http;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -34,17 +24,32 @@ import org.apache.http.util.CharsetUtils;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class HttpUtil {
 
     private static Logger logger = Logger.getLogger(HttpUtil.class);
 
     /**
-     * 
-     * @param reqURL 请求地址
-     * @param data 请求参数应该是 name1=value1&name2=value2 的形式
+     * @param reqURL      请求地址
+     * @param data        请求参数应该是 name1=value1&name2=value2 的形式
      * @param contentType
      * @return
      */
@@ -76,7 +81,6 @@ public class HttpUtil {
     }
 
     /**
-     * 
      * @param reqURL 请求参数应该是 name1=value1&name2=value2 的形式
      * @return
      */
@@ -157,7 +161,7 @@ public class HttpUtil {
 
     /**
      * 上传文件
-     * 
+     *
      * @throws ParseException
      * @throws IOException
      */
@@ -210,8 +214,8 @@ public class HttpUtil {
 
     /**
      * 下载文件
-     * 
-     * @param url http://www.xxx.com/img/333.jpg
+     *
+     * @param url          http://www.xxx.com/img/333.jpg
      * @param destFileName xxx.jpg/xxx.png/xxx.txt
      * @throws ClientProtocolException
      * @throws IOException
@@ -236,4 +240,179 @@ public class HttpUtil {
         // 关闭低层流。
         in.close();
     }
+
+    public static String getRequestJsonString(HttpServletRequest request) {
+        String submitMehtod = request.getMethod();
+        // GET
+        if (submitMehtod.equals("GET")) {
+            try {
+                return new String(request.getQueryString().getBytes(
+                        "iso-8859-1"), "utf-8").replaceAll("%22", "\"");
+            } catch (UnsupportedEncodingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            // POST
+        } else {
+            try {
+                return getRequestPostStr(request);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return "";
+    }
+
+    protected static byte[] getRequestPostBytes(HttpServletRequest request)
+            throws IOException {
+        int contentLength = request.getContentLength();
+        if (contentLength < 0) {
+            return null;
+        }
+        byte buffer[] = new byte[contentLength];
+        for (int i = 0; i < contentLength; ) {
+
+            int readlen = request.getInputStream().read(buffer, i,
+                    contentLength - i);
+            if (readlen == -1) {
+                break;
+            }
+            i += readlen;
+        }
+        return buffer;
+    }
+
+    protected static String getRequestPostStr(HttpServletRequest request)
+            throws IOException {
+        byte buffer[] = getRequestPostBytes(request);
+        String charEncoding = request.getCharacterEncoding();
+        if (charEncoding == null) {
+            charEncoding = "UTF-8";
+        }
+        return new String(buffer, charEncoding);
+    }
+
+    public static String getParameterMap(HttpServletRequest request) {
+        Map map = request.getParameterMap();
+        String text = "";
+        if (map != null) {
+            Set set = map.entrySet();
+            Iterator iterator = set.iterator();
+            while (iterator.hasNext()) {
+                Map.Entry entry = (Map.Entry) iterator.next();
+                if (entry.getValue() instanceof String[]) {
+                    logger.info("==A==entry的key： " + entry.getKey());
+                    String key = (String) entry.getKey();
+                    if (key != null && !"id".equals(key) && key.startsWith("[")
+                            && key.endsWith("]")) {
+                        text = (String) entry.getKey();
+                        break;
+                    }
+                    String[] values = (String[]) entry.getValue();
+                    for (int i = 0; i < values.length; i++) {
+                        logger.info("==B==entry的value: " + values[i]);
+                        key += "=" + values[i];
+                    }
+                    if (key.startsWith("[") && key.endsWith("]")) {
+                        text = (String) entry.getKey();
+                        break;
+                    }
+                } else if (entry.getValue() instanceof String) {
+                    logger.info("==========entry的key： " + entry.getKey());
+                    logger.info("==========entry的value: " + entry.getValue());
+                }
+            }
+        }
+        return text;
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public static String getParamNames(HttpServletRequest request) {
+        Map map = new HashMap();
+        Enumeration paramNames = request.getParameterNames();
+        while (paramNames.hasMoreElements()) {
+            String paramName = (String) paramNames.nextElement();
+
+            String[] paramValues = request.getParameterValues(paramName);
+            if (paramValues.length == 1) {
+                String paramValue = paramValues[0];
+                if (paramValue.length() != 0) {
+                    map.put(paramName, paramValue);
+                }
+            }
+        }
+
+        Set<Map.Entry<String, String>> set = map.entrySet();
+        String text = "";
+        for (Map.Entry entry : set) {
+            logger.info(entry.getKey() + ":" + entry.getValue());
+            text += entry.getKey() + ":" + entry.getValue();
+            logger.info("text------->" + text);
+        }
+        if (text.startsWith("[") && text.endsWith("]")) {
+            return text;
+        }
+        return "";
+    }
+
+    public void Security() {
+        System.setProperty("javax.net.ssl.trustStore", "D:/ssl/ssl.keystore");
+        System.setProperty("javax.net.ssl.trustStorePassword", "111111");
+        System.setProperty("javax.net.ssl.keyStore", "D:/ssl/ssl.keystore");
+        System.setProperty("javax.net.ssl.keyStorePassword", "111111");
+        // System.setProperty("https.protocols", "SSLv3");
+    }
+
+    /**
+     * 发送HTTPs请求,需要加载SSL安全证书
+     *
+     * @param reqURL
+     * @param data
+     * @return
+     */
+    public String sendHttps(String reqURL, String data) {
+        Security();
+        String respData = null;
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost method = new HttpPost(reqURL);
+        StringEntity entity = new StringEntity(data, "utf-8");//
+        entity.setContentEncoding("UTF-8");
+        entity.setContentType("application/json");
+        method.setEntity(entity);
+        try {
+            HttpResponse resp = httpClient.execute(method);
+            logger.info("sendHttps -- Recv Protocal !");
+            HttpEntity HttpResp = resp.getEntity();
+            respData = EntityUtils.toString(HttpResp);
+            logger.info("sendHttps -- The respData : " + respData);
+        } catch (Exception e) {
+            logger.error("sendHttps -- Send error !,The message :"
+                    + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            // 锟酵凤拷锟斤拷锟斤拷
+            method.releaseConnection();
+        }
+        return respData;
+    }
+
+    public String getReqString(HttpServletRequest request) {
+        logger.info("---------------getReqString-------------");
+        StringBuffer jb = new StringBuffer();
+        String line = null;
+        try {
+            BufferedReader reader = request.getReader();
+            while ((line = reader.readLine()) != null)
+                jb.append(line);
+            logger.debug("req data : " + jb.toString());
+            return jb.toString();
+        } catch (Exception e) {
+            // crash and burn
+            // throw new IOException("Error parsing JSON request string");
+            logger.error("ToString is error . error : " + e.getMessage());
+        }
+        return "";
+    }
+
 }
